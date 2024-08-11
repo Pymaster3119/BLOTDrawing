87a3389a574893f22ca257efcb92411f59766147
pymaster9 <adi31tya@icloud.com>
2024-08-11 11:39:31 -0400

    Making my blotinator's code smaller

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import matplotlib.pyplot as plt
 from scipy.ndimage import convolve
 import math
 import tqdm
+from multiprocessing import Pool
+import time
 
 #Load image
 image = Image.open('image.png').convert("L")
 image_array = np.array(image)
 
-plt.imshow(image_array)
+plt.imshow(image_array, cmap='gray')
 plt.axis('off')
 plt.show()
 
@@ -17,19 +19,32 @@ plt.show()
 print(image_array.shape)
 big_array = np.zeros((image_array.shape[0] * 16, image_array.shape[0] * 16, 1))
 print(big_array.shape)
-for x in tqdm.tqdm(range(image_array.shape[0])):
-    for y in range(image_array.shape[1]):
-        #Get intensity
-        intensity = image_array[x][y].item()/16
 
-        #Draw circle with appropriate radius
-        for i in range(int(-intensity), int(intensity) + 1):
-            for j in range(int(-intensity), int(intensity) + 1):
-                if math.sqrt(i**2 + j**2) <= intensity:
-                    big_array[16 * x + i][16 * y + j] = 1
+def process(minx):
+    maxx = minx + 10
+    array_slice = np.zeros((10, image_array.shape[0] * 16))
+    for x in range(minx, maxx): 
+        try:
+            _ = image_array[x]
+        except:
+            break
+        for y in range(image_array.shape[1]):
+            #Get intensity
+            intensity = image_array[x][y].item()/16
+
+            #Draw circle with appropriate radius
+            for i in range(int(-intensity), int(intensity) + 1):
+                for j in range(int(-intensity), int(intensity) + 1):
+                    if math.sqrt(i**2 + j**2) <= intensity:
+                        array_slice[i][16 * y + j] = 1
+    return array_slice
+
+with Pool() as p:
+    results = p.map(process, range(0, image_array.shape[0], 10))
+    print(results)
 
 
-plt.imshow(big_array)
+plt.imshow(big_array, cmap='gray')
 plt.axis('off')
 plt.show()
 
@@ -40,6 +55,17 @@ with open("Blotcode.js", "w") as txt:
     txt.write("const finalLines = [];\n")
     for x in tqdm.tqdm(range(big_array.shape[0])):
         for y in range(big_array.shape[1]):
-           if big_array[x][y] == 1:
-               txt.write(f"finalLines.push([[{x}, {y}]]);\n")
+            if big_array[x][y] == 1:
+                #Find line if applicable - along x axis
+                maxx = 0
+                for i in range(big_array.shape[0]):
+                    try:
+                        if big_array[x + i][y] == 1:
+                            maxx = x + i
+                            big_array[maxx][y] = 0
+                        else:
+                            break
+                    except:
+                        break
+                txt.write(f"finalLines.push([[{y}, {x}], [{y}, {maxx}]]);\n")
     txt.write("drawLines(finalLines);")
